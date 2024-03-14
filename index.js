61bc27bd99707719b8c54dc8d7c1ba6cdecc037f
Goutam Singla <122777044+Goutamsingla01@users.noreply.github.com>
2024-03-14 19:48:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const mongoose = require('mongoose');
+const port=3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/portfolio');
+var clientSchema = new mongoose.Schema({
+    name: String,
+    emailid: String,
+    concern: String
+});
+const client = mongoose.model('client', clientSchema);
+app.use('/static',express.static('static'))
+app.use(express.urlencoded())
+// app.set('views',path.join(__dirname,'views'))  this is used for template engine 
+app.set('view engine', 'pug')
+app.set('views',path.join(__dirname,'views'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+})
+app.get('/message',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/contact.html'));
+})
+app.get('/projectslist',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/projectslist.html'));
+})
+app.post('/message',(req,res)=>{
+    var mydata=new client(req.body)
+    var name=req.body.name;
+    mydata.save().then(()=>{ 
+        res.status(201).render('messagerec.pug',{data: name} );
+    }).catch(()=>{
+        res.status(400).send('item not saved')
+    })
+})
+function one(){
+    console.log("received");
+}
+app.listen(port,()=>{
+    console.log(`server is listening at port :${port}`)
+})
